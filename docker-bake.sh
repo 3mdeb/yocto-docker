508c9de6781db8f7bf3afafc68e0cacbc6d39a1b
Maciej Pijanowski <maciej.pijanowski@3mdeb.com>
2016-10-23 13:53:40 +0200

    docker-bake.sh: initial commit

---
+++ docker-bake.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker run --rm -it \
+-v $(pwd):$(pwd) \
+-v ~/.ssh:/home/build/.ssh \
+-v ~/.gitconfig:/home/build/.gitconfig \
+3mdeb/yocto-docker \
+/bin/bash -c "cd $(pwd) && source oe-init-build-env && bitbake $1"
