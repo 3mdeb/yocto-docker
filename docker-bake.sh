ece35f681a14e7c33faba65f543791d822863100
Maciej Pijanowski <maciej.pijanowski@3mdeb.com>
2017-04-25 14:40:47 +0200

    docker-bake.sh: set StrictHostKeyCheck=no during ssh session

--- docker-bake.sh
+++ docker-bake.sh
@@ -5,4 +5,5 @@ docker run --rm -it \
 -v $(dirname $SSH_AUTH_SOCK):$(dirname $SSH_AUTH_SOCK) -e SSH_AUTH_SOCK=$SSH_AUTH_SOCK  \
 -v ~/.gitconfig:/home/build/.gitconfig \
 3mdeb/yocto-docker \
-/bin/bash -c "ssh -T git@github.com; cd $(pwd) && source oe-init-build-env && bitbake $*"
+/bin/bash -c "ssh -T -o StrictHostKeyChecking=no git@github.com; \
+              cd $(pwd) && source oe-init-build-env && bitbake $*"
